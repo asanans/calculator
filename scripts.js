c954a6e636e32b20f7eb1a640624164a6cd3ef85
asanans <111622571+asanans@users.noreply.github.com>
2024-03-14 19:56:12 -0400

    Create operate function (takes an operator and 2 numbers as arguments and returns the result of the mathematical operation

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+let firstNumber
+let secondNumber
+let operator
+
 function add(a, b) {
     return a + b
 }
@@ -12,4 +16,8 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b
+}
+
+function operate(operator, a, b) {
+    return operator(a, b)
 }
